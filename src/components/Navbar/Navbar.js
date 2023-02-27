26b4563afa35e4fc4406ad4b05de20a4863a1bbc
Milinda Kaushika <milindakaushika@outlook.com>
2023-02-26 20:20:50 -0800

    change nevbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,45 +1,80 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import './navbar.css'
 import { BsCartPlus } from 'react-icons/bs'
 import { FaBars, FaTimes } from 'react-icons/fa'
 
-const Navbar = () => { 
-const [click, setClick] = useState(false)
-const handleClick = () => {
-    setClick(!click)
-}
 
-const [transparent, setTransparent] = useState('header')
-const addBg = () => {
-    if (window.scrollY >= 100){
-        setTransparent('header sticky')
-    }else{
-        setTransparent('header')
+const newlink = [
+    {
+        path: '#',
+        display: 'Home'
+    },
+    {
+        path: '#about',
+        display: 'About'
+    },
+    {
+        path: '#shop',
+        display: 'Shop'
+    },
+    {
+        path: '#services',
+        display: 'Services'
+    },
+    {
+        path: '#contacts',
+        display: 'Contacts'
+    },
+]
+
+
+const Navbar = () => {
+    const [click, setClick] = useState(false)
+    const handleClick = () => {
+        setClick(!click)
     }
-}
-window.addEventListener('scroll',addBg)
+
+    const [transparent, setTransparent] = useState('header')
+    const addBg = () => {
+        if (window.scrollY >= 100) {
+            setTransparent('header sticky')
+        } else {
+            setTransparent('header')
+        }
+    }
+    window.addEventListener('scroll', addBg)
     return (
         <header className={transparent}>
             <a href='/#' className="logo">W<span>A</span>TCHES</a>
             <ul className={click ? "navbar active" : "navbar"}>
-                <li onClick={handleClick}><a href='/#'>Home</a></li>
-                <li onClick={handleClick}><a href='/#about'>About</a></li>
-                <li onClick={handleClick}><a href='/#shop'>Shop</a></li>
-                <li onClick={handleClick}><a href='/#services'>Services</a></li>
-                <li onClick={handleClick}><a href='/#contacts'>Contacts</a></li>
-         
+                {
+                    newlink.map((item)=> {
+                       
+                        return (
+                      
+            
+                        <li onClick={handleClick}><a href={item.path}>{item.display}</a></li>
+
+
+                        )
+                    }
+                    
+                
+                )}
+
+
             </ul>
             <div className="nav-list">
                 <div className="btn">Register now</div>
                 <a href='/#'><BsCartPlus className='cart' /></a>
-          
-            <div className="menu-list" onClick={handleClick}>
-            
-                {
-                    click ? ( <FaTimes className="menu"/> ) : ( <FaBars className="menu"/>)
-                }
-             
-             </div>
+
+                <div className="menu-list" onClick={handleClick}>
+
+                    {
+                        click ? (<FaTimes className="menu" />) : (<FaBars className="menu" />)
+                    }
+
+                </div>
             </div>
         </header>
     )
